27e7971d521e1467a5021b45bdfdfbfac58e425f
MR-HOUS3z <74014924+MR-HOUS3z@users.noreply.github.com>
2020-11-05 14:54:10 -0600

    Update shit_macChanger.py

--- shit_macChanger.py
+++ shit_macChanger.py
@@ -1,1 +1,8 @@
+import subprocess 
 
+interface = wlan0
+print("[$]-Changing MAC addr for " + interface)
+
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:33:22:44:55", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
