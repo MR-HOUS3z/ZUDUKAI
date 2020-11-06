f66c8d26e4a09450efb280c068f22ea38d2d9671
MR-HOUS3z <74014924+MR-HOUS3z@users.noreply.github.com>
2020-11-06 08:06:18 -0600

    Update NETWRK_SCNNR.py

--- NETWRK_SCNNR.py
+++ NETWRK_SCNNR.py
@@ -1,25 +1,8 @@
 
-#
-# Smbtouch Scanner
-# By: 3gstudent
-# License: BSD 3-Clause
-'''
-Automatically scan the inner network.
-Use Smbtouch.exe to detect whether the target is vulnerable.
-Protocol: SMB
-Scan port:445
-Note:
-   You can also use protocl NBT and port 139,just change the Smbtouch-1.1.1.xml
-You can get Smbtouch.exe from this:
-https://github.com/x0rz/EQGRP_Lost_in_Translation/blob/master/windows/touches/Smbtouch-1.1.1.exe
-'''
 import os
 import fileinput
-#Start IP,change it
 BeginIP = '192.168.1.1'
-#End IP,change it
 EndIP = '192.168.1.10'
-#Log file
 fp = open('log.txt', 'w+')
 
 OldIP = '      <value>127.0.0.1</value>'
